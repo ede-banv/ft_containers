d3f344577a084670ab8953c7413c4047db0d46d0
Emma DE BANVILLE <ede-banv@student.42.fr>
2021-06-25 18:31:01 +0200

    start to vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -3,16 +3,31 @@
 
 # include <iostream>
 
-template < class T, class Alloc = allocator<T> >
+namespace ft {
+
+template < class T, class Alloc = std::allocator<T> >
 class vector
 {
 	public:
-		typedef T	value_type;
-		typedef Alloc	allocator_type;
-		typedef allocator_type::reference	reference;
+		typedef T								value_type;
+		typedef Alloc							allocator_type;
+		typedef allocator_type::reference		reference;
 		typedef allocator_type::const_reference	const_reference;
-		typedef allocator_type::pointer	pointer;
+		typedef allocator_type::pointer			pointer;
 		typedef allocator_type::const_pointer	const_pointer;
+
+		explicit vector (const allocator_type& alloc = allocator_type());
+		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+		template <class InputIterator>
+        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+copy (4)
+vector (const vector& x);
+	private:
+		value_type*	val;
+		allocator_type	alloc;
+
 };
+
+}
 
 #endif
