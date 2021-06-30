719ba7b579b62a097a7642fc41ffb3641c710e5b
Emma DE BANVILLE <ede-banv@student.42.fr>
2021-06-30 20:51:57 +0200

    declaration of vector class started: missing all iterator classes

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -15,16 +15,97 @@ class vector
 		typedef allocator_type::const_reference	const_reference;
 		typedef allocator_type::pointer			pointer;
 		typedef allocator_type::const_pointer	const_pointer;
+		typedef ptrdiff_t						difference_type;
+		typedef size_t							size_type;
 
-		explicit vector (const allocator_type& alloc = allocator_type());
-		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+		// ** MEMBER FUNCTIONS **
+
+		//Constructs an empty container, with no elements.
+		explicit vector (const allocator_type& alloc = allocator_type()): _alloc(alloc) {}
+		//Constructs a container with n elements. Each element is a copy of val.
+		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): _alloc(alloc)
+		{
+			_data = new value_type[n];
+		}
+		//Constructs a container with as many elements as the range [first,last), with each element constructed
+		//from its corresponding element in that range, in the same order.
 		template <class InputIterator>
         vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-copy (4)
-vector (const vector& x);
+		//Constructs a container with a copy of each of the elements in x, in the same order.
+		vector (const vector& x);
+
+		//This destroys all container elements, and deallocates all the storage capacity allocated
+		//by the vector using its allocator.
+		virtual ~vector();
+
+		//Copies all the elements from x into the container. The container preserves its current allocator,
+		//which is used to allocate storage in case of reallocation.
+		vector& operator= (const vector& x);
+
+
+		// ** CAPACITY **
+		size_type	size() const;
+		size_type	max_size() const;
+		void		resize(size_type n, value_type val = value_type());
+		size_type	capacity() const;
+		bool		empty() const;
+		void		reserve(size_type n);
+
+		// ** ELEMENT ACCESS **
+		//Returns a reference to the element at position n in the vector container.
+		reference		operator[] (size_type n);
+		const_reference	operator[] (size_type n) const;
+		reference		at(size_type n);
+		const_reference	at(size_type n) const;
+		reference		front();
+		const_reference	front() const;
+		reference		back();
+		const_reference	back() const;
+
+		// ** MODIFIERS **
+		template <class InputIterator>
+		void		assign(InputIterator first, InputIterator last);
+		void		assign(size_type n, const value_type& val);
+		void		push_back(const value_type& val);
+		void		pop_back();
+		void		insert(iterator position, size_type n, const value_type& val);
+		template <class InputIterator>
+		void		insert(iterator position, InputIterator first, InputIterator last);
+		iterator	erase(iterator position);
+		iterator	erase(iterator first, iterator last);
+		void		swap(vector& x);
+		void		clear();
+
+		// ** RELATIONAL OPERATORS **
+		//compare lexicographiquement les valeurs dans le vector
+		template <class T, class Alloc>
+		bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc>
+		bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc>
+		bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc>
+		bool operator<= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc>
+		bool operator>  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc>
+		bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+
+		// ** ITERATORS **
+		iterator				begin();
+		const_iterator			begin() const;
+		iterator				end();
+		const_iterator			end() const;
+		reverse_iterator		rbegin();
+		const_reverse_iterator	rbegin() const;
+		reverse_iterator		rend();
+		const_reverse_iterator	rend() const;
 	private:
-		value_type*	val;
-		allocator_type	alloc;
+		value_type*				_data;
+		allocator_type			_alloc;
+		size_type				_size;
+		size_type				_capacity;
+		const static size_type	_max_size;
 
 };
 
